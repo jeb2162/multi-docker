7788d4c507ec3b3d8dd041f38c01614b4965e842
josh.brown <josh.brown@COMP10583.local>
2019-09-01 17:25:26 -0400

    aws deploy

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link>
+            <Link to="/">Home!!</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
